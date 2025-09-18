4f1fb669bcbbe51076c6659fdda67bd05b3f7805
Ivan Kraich <139285492+IvanKr8@users.noreply.github.com>
2025-09-18 20:03:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+BOT_TOKEN = "8275504974:AAEJblNngby0n-XEEUNn0nVe4y_BxAVEEsw"
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.reply("Привет!")
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
