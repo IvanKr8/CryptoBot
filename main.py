62ae1d341fda923c0b666eae25ec43ee94e87e5d
Ivan Kraich <139285492+IvanKr8@users.noreply.github.com>
2025-09-18 20:10:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ BOT_TOKEN = "8275504974:AAEJblNngby0n-XEEUNn0nVe4y_BxAVEEsw"
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет!")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
