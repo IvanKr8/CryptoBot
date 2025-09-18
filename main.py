84822a0900ca9bbe7047dff7a525dcc663b48620
Ivan Kraich <139285492+IvanKr8@users.noreply.github.com>
2025-09-18 20:07:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = "8275504974:AAEJblNngby0n-XEEUNn0nVe4y_BxAVEEsw"
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет!")
 
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    await message.reply("Привет!")
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
 
-if __name__ == "__main__":
-    executor.start_polling(dp)
+app.run_polling()
