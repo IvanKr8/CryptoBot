138a808ff53f5a4ca6f506589ccd680ccf3a827a
Ivan Kraich <139285492+IvanKr8@users.noreply.github.com>
2025-09-21 15:27:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import asyncio
 from detector import start_detector
+from notifier import log_info, log_error
 
 async def main():
-    await start_detector()
+    await log_info("🚀 Detector бот стартует...")
+    try:
+        await start_detector()
+    except Exception as e:
+        await log_error(f"Бот упал с ошибкой: {str(e)}")
 
 if __name__ == "__main__":
     asyncio.run(main())
