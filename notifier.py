c0d83169ed83234ca2c6f456147e42bc51ffa017
Ivan Kraich <139285492+IvanKr8@users.noreply.github.com>
2025-09-21 15:26:40 +0200

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,12 +1,19 @@
 import os
 from dotenv import load_dotenv
 from telegram import Bot
+import asyncio
 
 load_dotenv()
+
+# Сигналы
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+bot_signals = Bot(token=TELEGRAM_TOKEN)
 
-bot = Bot(token=TELEGRAM_TOKEN)
+# Логи
+TELEGRAM_LOG_TOKEN = os.getenv("TELEGRAM_LOG_TOKEN")
+TELEGRAM_LOG_CHAT_ID = os.getenv("TELEGRAM_LOG_CHAT_ID")
+bot_logs = Bot(token=TELEGRAM_LOG_TOKEN)
 
 async def send_signal(symbol, side, price_level, volume, sl, tp1, tp2, confidence):
     msg = (
@@ -19,4 +26,10 @@ async def send_signal(symbol, side, price_level, volume, sl, tp1, tp2, confidenc
         f"TP2: {tp2}\n"
         f"Уверенность: {confidence:.2f}"
     )
-    await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=msg)
+    await bot_signals.send_message(chat_id=TELEGRAM_CHAT_ID, text=msg)
+
+async def log_info(message):
+    await bot_logs.send_message(chat_id=TELEGRAM_LOG_CHAT_ID, text=f"ℹ️ {message}")
+
+async def log_error(message):
+    await bot_logs.send_message(chat_id=TELEGRAM_LOG_CHAT_ID, text=f"❌ {message}")
