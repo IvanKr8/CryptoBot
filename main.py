6de8ff02194fd47d97770c9a19517822bff2b4ac
Ivan Kraich <139285492+IvanKr8@users.noreply.github.com>
2025-09-24 13:42:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import asyncio
-from detector import start_detector
+from detector import whales_detector
 from notifier import log_info, log_error
 
 async def main():
     await log_info("🚀 Detector бот стартует...")
     try:
-        await start_detector()
+        await whales_detector()
     except Exception as e:
         await log_error(f"Бот упал с ошибкой: {str(e)}")
 
