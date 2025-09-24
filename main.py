36999065e00682f17ae859835caa62e820b5c524
Ivan Kraich <139285492+IvanKr8@users.noreply.github.com>
2025-09-24 16:17:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import asyncio
-from detector import fetch_whales
+from detector import fetch_whales, send_periodic_log
 from notifier import log_info, log_error
 
 async def main():
     await log_info("🚀 Whale Radar стартует...")
     try:
-        await fetch_whales()
+        task1 = asyncio.create_task(fetch_whales())
+        task2 = asyncio.create_task(send_periodic_log())
+        await asyncio.gather(task1, task2)
     except Exception as e:
         await log_error(f"Бот упал: {e}")
 
