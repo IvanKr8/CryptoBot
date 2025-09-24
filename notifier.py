44e7c7b5097549286f22f1861f0921113e9d99e6
Ivan Kraich <139285492+IvanKr8@users.noreply.github.com>
2025-09-24 15:25:47 +0200

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -5,26 +5,21 @@ import asyncio
 
 load_dotenv()
 
-# Сигналы
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 bot_signals = Bot(token=TELEGRAM_TOKEN)
 
-# Логи
 TELEGRAM_LOG_TOKEN = os.getenv("TELEGRAM_LOG_TOKEN")
 TELEGRAM_LOG_CHAT_ID = os.getenv("TELEGRAM_LOG_CHAT_ID")
 bot_logs = Bot(token=TELEGRAM_LOG_TOKEN)
 
-async def send_signal(symbol, side, price_level, volume, sl, tp1, tp2, confidence):
+async def send_signal(symbol, side, price, volume_usd, rel_depth, confidence):
     msg = (
-        f"🚨 Сигнал по {symbol.upper()}\n"
-        f"Side: {side}\n"
-        f"Цена уровня: {price_level}\n"
-        f"Объем: {volume}\n"
-        f"SL: {sl}\n"
-        f"TP1: {tp1}\n"
-        f"TP2: {tp2}\n"
-        f"Уверенность: {confidence:.2f}"
+        f"🚨 {side} SIGNAL: {symbol}\n"
+        f"Price: {price}\n"
+        f"Volume USD: {volume_usd:.2f}\n"
+        f"Relative Depth: {rel_depth:.2f}\n"
+        f"Confidence: {confidence:.2f}"
     )
     await bot_signals.send_message(chat_id=TELEGRAM_CHAT_ID, text=msg)
 
